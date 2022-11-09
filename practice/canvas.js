550df6827b0979526dfabbbab9fc62f56188482b
Luke Stamper <luke.stamper12@outlook.com>
2022-11-09 16:17:51 -0700

    for loop

--- practice/canvas.js
+++ practice/canvas.js
@@ -1,5 +1,42 @@
-
 let canvas = document.getElementById("canvas-1");
-let context = canvas.getContext();
+canvas.width = 100;
+canvas.height = 100;
+/** @type {CanvasRenderingContext2D} */
+let context = canvas.getContext("2d");
 
-context.fillRect(50, 50, 25, 25);
+let squares = [];
+let gridSize = 4;
+let size = canvas.width / gridSize;
+let colors = [
+	"red",
+	"orange",
+	"yellow",
+	"green",
+	"blue",
+	"indigo",
+	"purple"
+];
+
+for (let row = 0; row < gridSize; row++) {
+	for (let col = 0; col < gridSize; col++) {
+		let colorIndex = Math.floor(Math.random() * colors.length);
+		let colors = colors[colorIndex];
+		drawSquare(col * size, row * size, "red", size);
+	}
+}
+
+// drawSquare(25, 0, "blue");
+// drawSquare(50, 0, "green");
+// drawSquare(75, 0, "purple");
+
+function drawSquare(x, y, color, size = 25) {
+	let square = new Path2D();
+	square.rect(x, y, size, size);
+
+	squares.push(square);
+
+	context.fillStyle = color;
+	context.fillRect(x, y, size, size);
+}
+
+console.log(squares);
--- practice/variables.js
+++ practice/variables.js
@@ -1,21 +1,38 @@
-console.log("Hello World");
+console.log("Hello world!");
 
-// for typing in english not JavaScript
-let x = 500;
+// then I can write anything I want
+// and it wont get errors because it is a comment
+let x = 500; // set x to have the value 500
 let y = 276;
 
 let sum = x + y;
-
-let pi =  3.14;
+let pi = 3.14;
 
 console.log("x is ", x);
 
-let myname = "Luke Stamper"
+let myname = "Robert Hardy";
 
+// strings have a length property
 myname.length;
 
+let whatWillHappen = myname + x;
+
+console.log(whatWillHappen, sum);
+
+let crazMAth; // = (100 * x) / (sum + whatWillHappen) - y;
+
+console.log(crazMAth);
+
+let isGameOver = false;
+
+console.log(isGameOver, typeof isGameOver);
+
+// arrays can hold lists of values
 let ages = [25, 17, 16, 85];
+
 console.log("ages is an array", ages);
+
+let secondAge = ages[1];
 
 let student = {
 	name: "Toby",
@@ -26,8 +43,6 @@ let student = {
 };
 
 console.log("student is", student);
-console.log("student name is", student);
+console.log("student name is", student.name);
 
 student.gradeLevel = 9;
-
-
